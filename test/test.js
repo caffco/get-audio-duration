281df4d03a8e12dd7a92f45b6f926a245ea6876e
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2015-08-12 20:36:36 +0200

    Fixed path to fixture sound

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
-var assert = require( 'assert' );
+var assert = require( 'assert' ),
+	path = require( 'path' );
 
 var getDuration = require( '..' );
 
 it( 'should get duration', function () {
-	return getDuration( 'Rayman_2_music_sample.ogg' )
+	return getDuration( path.resolve( __dirname, './Rayman_2_music_sample.ogg' ) )
 		.then( function ( duration ) {
 			assert.equal( duration, 33.002676 );
 		} );
