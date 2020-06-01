b2fd99bf87a8937c986a54d824f3663981d646a2
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2020-06-01 18:06:30 +0200

    :white_check_mark: Add test case to check files with spaces in their path

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -12,6 +12,10 @@ const expectedFLACAudioDuration = 97;
 const expectedAudioDurationThreshold = 1;
 
 const sampleOGGFilePath = resolvePath(__dirname, './Rayman_2_music_sample.ogg');
+const sampleOGGWithSpacesFilePath = resolvePath(
+  __dirname,
+  './Rayman 2 music sample.ogg',
+);
 const sampleFLACFilePath = resolvePath(
   __dirname,
   './2L-125_stereo-44k-16b_04.flac',
@@ -59,6 +63,16 @@ describe('get-audio-duration', function () {
     it('Should throw an error if not an audio file', async function () {
       const durationPromise = getDuration(resolvePath(__dirname, __filename));
       await expect(durationPromise).to.be.eventually.rejected;
+    });
+
+    it('Should return proper duration if file contains spaces', async function () {
+      const duration = await getDuration(sampleOGGWithSpacesFilePath);
+      expect(duration)
+        .to.be.a('number')
+        .that.is.closeTo(
+          expectedOGGAudioDuration,
+          expectedAudioDurationThreshold,
+        );
     });
   });
 });
