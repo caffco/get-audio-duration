accdd7db3c33b00ebcea98b2113cecf2ff01ce44
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2023-05-27 10:04:23 +0200

    :boom: Drop support for streams

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import * as ffprobe from '@ffprobe-installer/ffprobe'
 
-import * as execa from 'execa'
-import * as isStream from 'is-stream'
-import { Readable as ReadableStream } from 'stream'
+import execa from 'execa'
 
 const getFFprobeWrappedExecution = (
-  input: string | ReadableStream,
+  input: string,
   ffprobePath?: string
 ): execa.ExecaChildProcess => {
   const params = [
@@ -19,26 +17,15 @@ const getFFprobeWrappedExecution = (
 
   const overridenPath = ffprobePath || ffprobe.path
 
-  if (typeof input === 'string') {
-    return execa(overridenPath, [...params, input])
-  }
-
-  if (isStream(input)) {
-    return execa(overridenPath, [...params, '-i', 'pipe:0'], {
-      reject: false,
-      input,
-    })
-  }
-
-  throw new Error('Given input was neither a string nor a Stream')
+  return execa(overridenPath, [...params, input])
 }
 
 /**
  * Returns a promise that will be resolved with the duration of given audio in
  * seconds.
  *
- * @param input Stream or path to file to be used as
- * input for `ffprobe`.
+ * @param absolutePathToFile Absolute path to file to be used as input for
+ * `ffprobe`.
  * @param [ffprobePath] Optional. Path to `ffprobe` binary. Do not provide any
  * value for this parameter unless you need to override the path to `ffprobe`.
  * Defaults to the path provided by `@ffprobe-installer/ffprobe`, which works in
@@ -48,10 +35,13 @@ const getFFprobeWrappedExecution = (
  * seconds.
  */
 const getAudioDurationInSeconds = async (
-  input: string | ReadableStream,
+  absolutePathToFile: string,
   ffprobePath?: string
 ): Promise<number> => {
-  const { stdout } = await getFFprobeWrappedExecution(input, ffprobePath)
+  const { stdout } = await getFFprobeWrappedExecution(
+    absolutePathToFile,
+    ffprobePath
+  )
   const matched = stdout.match(/duration="?(\d*\.\d*)"?/)
   if (matched && matched[1]) return parseFloat(matched[1])
   throw new Error('No duration found!')
